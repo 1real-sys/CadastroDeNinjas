328b6d39fe74cd1f53b162932379663942a44fd1
1real-sys <cauan.anna@edu.ufes.br>
2025-10-11 23:29:32 -0300

    feat: lombok adicionado

--- src/main/java/dev/teamwin/CadastroDeNinjas/Missoes/Model/MissoesModel.java
+++ src/main/java/dev/teamwin/CadastroDeNinjas/Missoes/Model/MissoesModel.java
@@ -4,10 +4,17 @@ import dev.teamwin.CadastroDeNinjas.Ninjas.Model.NinjaModel;
 import jakarta.persistence.*;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
     @Id
