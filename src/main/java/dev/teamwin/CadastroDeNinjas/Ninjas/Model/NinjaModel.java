e337660c3e5df42a8efc6589cd3ecfee68801f84
1real-sys <cauan.anna@edu.ufes.br>
2025-10-13 22:21:25 -0300

    fix: Adiciona o @Column na minha classe

--- src/main/java/dev/teamwin/CadastroDeNinjas/Ninjas/Model/NinjaModel.java
+++ src/main/java/dev/teamwin/CadastroDeNinjas/Ninjas/Model/NinjaModel.java
@@ -20,13 +20,18 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
-
+    @Column(name = "nome")
     private String nome;
 
     @Column(unique = true)
     private String email;
+
+    @Column(name = "idade")
     private int idade;
+    @Column(name = "img_url")
+    private String imgUrl;
 
 
     // Relacionamento entre as tabelas, uma missao pode ter varios ninjas.
