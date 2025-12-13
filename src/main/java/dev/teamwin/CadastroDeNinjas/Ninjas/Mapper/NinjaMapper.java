2392b1c94db835f41086ba37bd0193cce9856532
1real-sys <cauan.anna@edu.ufes.br>
2025-12-13 02:23:30 -0300

    Chore: inclui a versão do dto pela entidade e mapeia manualmente no NinjaMapper os caminhos entidade x dto

--- src/main/java/dev/teamwin/CadastroDeNinjas/Ninjas/Mapper/NinjaMapper.java
+++ src/main/java/dev/teamwin/CadastroDeNinjas/Ninjas/Mapper/NinjaMapper.java
@@ -1,4 +1,35 @@
 package dev.teamwin.CadastroDeNinjas.Ninjas.Mapper;
 
+import dev.teamwin.CadastroDeNinjas.Ninjas.DTO.NinjaDTO;
+import dev.teamwin.CadastroDeNinjas.Ninjas.Model.NinjaModel;
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDTO) {
+         NinjaModel ninjaModel = new NinjaModel();
+         ninjaModel.setId(ninjaDTO.getId());
+         ninjaModel.setNome(ninjaDTO.getNome());
+         ninjaModel.setEmail(ninjaDTO.getEmail());
+         ninjaModel.setIdade(ninjaDTO.getIdade());
+         ninjaModel.setImgUrl(ninjaDTO.getImgUrl());
+         ninjaModel.setRanking(ninjaDTO.getRanking());
+         ninjaModel.setMissoes(ninjaDTO.getMissoes());
+
+         return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel) {
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setNome(ninjaModel.getNome());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setIdade(ninjaModel.getIdade());
+        ninjaDTO.setImgUrl(ninjaModel.getImgUrl());
+        ninjaDTO.setRanking(ninjaModel.getRanking());
+        ninjaDTO.setMissoes(ninjaModel.getMissoes());
+
+        return ninjaDTO;
+    }
 }
